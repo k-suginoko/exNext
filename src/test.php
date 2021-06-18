170f8943d8e2e259850a192d9043473065fee23c
kana.sugihara <kana.sugihara@brewus.co.jp>
2021-06-18 16:32:05 +0900

    no message

--- src/test.php
+++ src/test.php
@@ -1,5 +1,9 @@
 <?php
+header('Access-Control-Allow-Methods: POST, PUT, GET, POST, DELETE, OPTIONS, PUT, GET, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Origin: *, *, *');
 
 $word1 = $_POST['text']
- 
+$params = json_decode(file_get_contents('php://input'), true);
+
 echo "{$_POST['text']}を入力しました。";
+echo "params: {$params}";
