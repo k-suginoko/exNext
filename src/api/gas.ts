943447a93d443a559585fb881280e8dd030494bf
kana.sugihara <kana.sugihara@brewus.co.jp>
2021-07-05 17:23:10 +0900

    no message

--- src/api/gas.ts
+++ src/api/gas.ts
@@ -4,9 +4,10 @@ import { get } from 'lib/axios'
 const getParams: object = {
   method: 'GET',
   mode : "no-cors",
-  // headers:{
-  //   "Content-Type": "application/json"
-  // }
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin':'*'
+  }
 }
 
 // const getParams: object = {
@@ -26,7 +27,7 @@ class GasApi {
    * ガワだけ用意したGasのAPI
    */
   async fetchAll () {
-    return await fetch('https://script.google.com/a/macros/brewus.co.jp/s/AKfycbwRvLQq4-lyJZ6LtUlu4kbIN4qTwNDBzAjIxglmaDHCz5ZjDh7hILDGoi76MvsFhWnVwA/exec', getParams)
+    return await fetch('https://script.google.com/a/macros/brewus.co.jp/s/AKfycbyUJtRgXQTugpR-go9WvN1opqNp7eOevU354LlbeSiHxKUfKGq7E72BMwxEmDh61fLOiQ/exec', getParams)
     .then(res => {
       console.log('ress', res)
     })
